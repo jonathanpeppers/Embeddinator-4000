9bedb31be7dde8ba277d8d608782eb916b737183
Jonathan Peppers <jonathan.peppers@microsoft.com>
2017-07-18 11:43:09 -0500

    [java] prototype of a C# interface generating a C# invoker
    This is what I had to do:
    - add submodule of java.interop
    - add generator, Java.Interop.CustomNamingAttributes,
    Java.Interop.Tools.Cecil, Java.Interop.Tools.Diagnostics,
    Java.Interop.Tools.TypeNameMappings, Xamarin.Android.Cecil,
    Xamarin.Android.Tools.AnnotationSupport,
    Xamarin.Android.Tools.ApiXmlAdjuster to our SLN
    
    In my test:
    - Compiled `IJavaCallback.cs` to an assembly
    - Loaded it with Xamarin.Android’s version of Mono.Cecil
    - Found the `TypeDefinition` and ran it through `ManagedInterfaceGen`
    - Had to add `[Register]` attribute to `IJavaCallback`
    
    Note:
    - `gen.Validate` returns false, it is having trouble looking up
    `IJavaObject` — but it still works
    - a `partial` `IJavaCallback` interface gets generated…

---
+++ tests/MonoEmbeddinator4000.Tests/InterfaceGenTest.cs
@@ -0,0 +1,52 @@
+﻿using System.CodeDom.Compiler;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Mono.Cecil;
+using MonoDroid.Generation;
+using NUnit.Framework;
+
+namespace MonoEmbeddinator4000.Tests
+{
+    [TestFixture]
+    public class InterfaceGenTest : TempFileTest
+    {
+        AssemblyDefinition LoadAssembly(string resourceFile)
+        {
+            var parameters = new CompilerParameters
+            {
+                OutputAssembly = temp,
+            };
+            parameters.ReferencedAssemblies.Add(XamarinAndroid.FindAssembly("System.dll"));
+            parameters.ReferencedAssemblies.Add(XamarinAndroid.FindAssembly("Mono.Android.dll"));
+
+            AssemblyGenerator.CreateFromResource(resourceFile, parameters);
+            return AssemblyDefinition.ReadAssembly(temp);
+        }
+
+        [Test]
+        public void Leeroy()
+        {
+            var dir = Path.GetDirectoryName(GetType().Assembly.Location);
+            var assembly = LoadAssembly("IJavaCallback");
+            var type = assembly.MainModule.Types.First(t => t.Name == "IJavaCallback");
+            var options = new CodeGenerationOptions();
+            var gen = new ManagedInterfaceGen(type);
+
+            gen.Validate(options, new GenericParameterDefinitionList());
+
+            string cs;
+            using (var stream = new MemoryStream())
+            {
+                using (var writer = new StreamWriter(stream))
+                {
+                    gen.Generate(writer, "\t", options, new GenerationInfo(dir, dir, temp));
+                }
+
+                cs = Encoding.Default.GetString(stream.ToArray());
+            }
+
+            Approvals.Approve(cs);
+        }
+    }
+}
---
+++ tests/MonoEmbeddinator4000.Tests/Samples/IJavaCallback.cs
@@ -0,0 +1,12 @@
+﻿using Android.Runtime;
+using Java.Interop;
+
+namespace MonoEmbeddinator4000.Tests
+{
+    [Register("mono.embeddinator.android.IJavaCallback", DoNotGenerateAcw = true)]
+    public interface IJavaCallback : IJavaObject
+    {
+        [Export("send"), Register("send", "(Ljava/lang/String;)V", "MonoEmbeddinator4000.Tests.IJavaCallbackInvoker")]
+        void Send(string text);
+    }
+}
